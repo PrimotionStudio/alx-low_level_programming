681f32d6a768dda21a77ac590a7b90d3d1e65540
Martins <oyedelenewton@gmail.com>
2023-09-09 22:16:18 -0700

    Variables If Else

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,32 +7,27 @@
   */
 int main(void)
 {
-	int num;
-	int subnum;
 	int num1;
 	int num2;
 	int num3;
 
-	for (num = 0; num <= 999; num++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		subnum = num;
-		num1 = subnum % 10;
-		subnum /= 10;
-		num2 = subnum % 10;
-		subnum /= 10;
-		 num3 = subnum % 10;
-
-		if (num1 < num2)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
-			if (num2 < num3)
+			for (num3 = 0; num3 <= 9; num3++)
 			{
-				putchar(num1 + '0');
-				putchar(num2 + '0');
-				putchar(num3 + '0');
-				if ((num1 != 7) || (num2 != 8) || (num3 != 9))
+				if ((num1 < num2) && (num2 < num3))
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
+
+					if ((num1 != 7) || (num2 != 8) || (num3 != 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 
 			}
