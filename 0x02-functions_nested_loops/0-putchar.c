fa6e3aa37678423b188c61a0d7c8961fc1efc68e
Martins <oyedelenewton@gmail.com>
2023-09-12 14:35:52 -0700

    1 down 10 to go

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+#include "_putchar.h"
+#endif
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
