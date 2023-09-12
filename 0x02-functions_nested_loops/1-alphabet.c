14730159a508691ab1dc843c339c3db273dbc958
Martins <oyedelenewton@gmail.com>
2023-09-12 14:46:36 -0700

    2 down 9 to go

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+#include "main.h"
+#endif
+
+void print_alphabet(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	while (start <= end)
+	{
+		_putchar(start);
+	}
+}
