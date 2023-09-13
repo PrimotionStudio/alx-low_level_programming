5499b48875bc63e63edcc0f6545c977880125503
Martins <oyedelenewton@gmail.com>
2023-09-12 17:07:41 -0700

    8 down 3 to go

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,13 +13,13 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
+	last_digit = num % 10;
+
 	if (num < 0)
 	{
 		num = -num;
 	}
 
-	last_digit = num % 10;
-
-	_putchar('0' + last_digit);
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
