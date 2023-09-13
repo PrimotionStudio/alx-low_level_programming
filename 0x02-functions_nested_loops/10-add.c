ed7748191dedfb7d0189b178188ae0916ca3cd60
Martins <oyedelenewton@gmail.com>
2023-09-12 20:16:32 -0700

    ALmost Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+#include "main.h"
+#endif
+
+/**
+  * add - function to add two int
+  * @a: first arg
+  * @b: second arg
+  * Return: Depends on the arguments
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
