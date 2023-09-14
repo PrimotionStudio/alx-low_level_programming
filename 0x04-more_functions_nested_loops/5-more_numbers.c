115114e3996d0059f4d84e252139dbeec53505df
Martins <oyedelenewton@gmail.com>
2023-09-14 11:15:16 -0700

    More functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,18 @@
   */
 void more_numbers(void)
 {
-	char c;
+	int c;
 	int i = 1;
 
 	while (i <= 10)
 	{
-		for (c = '0'; c <= '14'; c++)
+		for (c = 0; c <= 14; c++)
 		{
-			_putchar(c);
+			if (c >= 10)
+			{
+				_putchar((c / 10) + '0');
+			}
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 		i++;
