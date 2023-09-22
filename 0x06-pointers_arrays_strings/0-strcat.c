3ca7d0a549f20f83ea1a46fc16b44c3c86842f01
Martins <oyedelenewton@gmail.com>
2023-09-21 17:13:50 -0700

    String

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * strcat - Entry point
+  * @dest: arg1
+  * @src: arg2
+  * Return: concatinated string
+  */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0;
+	int slen = 0;
+
+	while (dest[dlen] != '\0')
+	{
+		dlen++;
+	}
+
+	while (src[slen] != '\0')
+	{
+		dest[dlen] = src[slen];
+		dlen++;
+		slen++;
+	}
+	dest[dlen] = '\0';
+
+	return (dest);
+}
