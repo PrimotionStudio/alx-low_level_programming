46718f46419a9b61631f7e9966c83face7989a77
Martins <oyedelenewton@gmail.com>
2023-09-28 10:10:43 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+  * _sqrt_ - Entry point
+  * @n: arg
+  * @guess: arg
+  * Return: ret
+  */
+int _sqrt_(int n, int guess);
+
+/**
+  * _sqrt_recursion - Entry Point
+  * @n: arg
+  * Return : ret
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	return (_sqrt_(n, 1));
+}
+
+int _sqrt_(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	if (guess * guess > n)
+	{
+		return  (-1);
+	}
+	return (_sqrt_(n, guess + 1));
+}
