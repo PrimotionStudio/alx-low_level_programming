4f8af076decdb59bce9add5cf928c6e0a610763f
Martins <oyedelenewton@gmail.com>
2023-09-28 08:53:24 -0700

    recursive

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * factorial - Entry point
+  * @n: arg
+  * Return: ret
+  */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
