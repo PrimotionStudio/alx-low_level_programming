1026824b8c0c2c05acbd8b5f34bcd91b28470104
Martins <oyedelenewton@gmail.com>
2023-10-03 15:18:17 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * @argc: arg
+  * @argv: arg
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
