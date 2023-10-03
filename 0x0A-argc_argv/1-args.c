119fe3cb1ce341fba1cc29af73650e9a0dcf534b
Martins <oyedelenewton@gmail.com>
2023-10-03 15:32:22 -0700

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * @argc: arg
+  * @argv: arg
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
