7ab8e5597137d33ad5e207e9949023005767c928
Martins <oyedelenewton@gmail.com>
2023-10-03 16:04:52 -0700

    arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  * @argc: arg
+  * @argv: arg
+  * Return: 0 or 1
+  */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%ld\n", (long)num1 * (long)num2);
+	return (0);
+}
