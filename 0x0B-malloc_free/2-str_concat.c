4a90c10f6e954b0cf698ef7a96061e803a7dd3fe
Martins <oyedelenewton@gmail.com>
2023-10-05 01:27:36 -0700

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * str_concat - Entry point
+  * @s1: arg
+  * @s2: arg
+  * Return: ret
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int len1;
+	int len2;
+	char *str;
+	int i;
+	int j;
+
+	len1 = 0;
+	len2 = 0;
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	str = (char *) malloc(1 + (sizeof(char) * (len1 + len2)));
+	for (i = 0; i < len1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		str[(len1 + j)] = s2[j];
+	}
+	str[(len1 + len2)] = '\0';
+
+	return (str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
+char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
