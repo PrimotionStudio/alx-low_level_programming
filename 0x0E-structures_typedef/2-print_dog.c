aa3e13b890de2467b16aa21edcd5a467e03be2c4
Martins <oyedelenewton@gmail.com>
2023-10-10 11:10:45 -0700

    qwertyuiop

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * print_dog - Entry point
+  * @d: arg
+  */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
