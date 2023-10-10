28968809b0fe13f8cab27de4f081b2b0662f94af
Martins <oyedelenewton@gmail.com>
2023-10-10 09:04:02 -0700

    Im Cool

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+  * struct dog - dog structure
+  * @name: arg
+  * @age: arg
+  * @owner: arg
+  * Description: stuff about dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
