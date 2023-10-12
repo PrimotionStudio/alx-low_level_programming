08692ea29f21b69aa82d5d1e51182fa79af1f6ab
Martins <oyedelenewton@gmail.com>
2023-10-12 11:08:00 -0700

    varadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,7 @@ void print_all(const char * const format, ...)
 	{
 		if (pflag != 0)
 			printf(", ");
-		switch(format[i])
+		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(str, int));
