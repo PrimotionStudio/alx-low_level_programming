df8387a9668d59c2c624465f8e018c88482989db
Martins <oyedelenewton@gmail.com>
2023-10-19 15:59:14 -0700

    Advance tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void _construct(void) __attribute__ ((constructor));
   */
 void _construct(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
