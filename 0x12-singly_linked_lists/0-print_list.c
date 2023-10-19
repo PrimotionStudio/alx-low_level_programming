14193b15aed396dc91d4d6df1e60287aa63db4b1
Martins <oyedelenewton@gmail.com>
2023-10-19 10:02:44 -0700

    I was able to print nodes of lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,10 +6,12 @@
 /**
   * print_list - Prints the values of a singly linked list
   * @h: list to be printed
+  * Return: Returns the number of nodes visited
   */
 size_t print_list(const list_t *h)
 {
 	size_t i = 1;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
