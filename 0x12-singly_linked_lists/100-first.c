43320a452427d378c69e6637e74b200a4dff4ae7
Martins <oyedelenewton@gmail.com>
2023-10-19 15:57:48 -0700

    Advance tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void _construct (void) __attribute__ ((constructor));
+void _construct(void) __attribute__ ((constructor));
 /**
   * _construct - This function is supposed to run before main
   */
-void _construct()
+void _construct(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
