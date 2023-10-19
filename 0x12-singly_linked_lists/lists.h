f4b606401f49169dcdea85e9febbed30a1072dd2
Martins <oyedelenewton@gmail.com>
2023-10-19 10:06:29 -0700

    I was able to print nodes of lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef MAINH
 #define MAINH
+#include <stddef.h>
 
 int _putchar(char c);
 
@@ -14,9 +14,9 @@ int _putchar(char c);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 
