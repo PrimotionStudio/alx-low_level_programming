e37ae58d9b530268d62af36e1e4d9835ff8a89f7
Martins <oyedelenewton@gmail.com>
2023-10-27 00:03:05 -0700

    First

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,3 +5,20 @@
   * @b: The string
   * Return: the coverted number
   */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i  = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (0);
+
+		num = (num << 1) + (b[i] - '0');
+		i++;
+	}
+	return (num);
+}
