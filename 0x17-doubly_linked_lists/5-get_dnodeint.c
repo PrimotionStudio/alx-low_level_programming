bb9f3cdf268280f6fde56c94aa66702242eae048
Martins <oyedelenewton@gmail.com>
2023-12-07 04:44:12 -0800

    5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *hd = head, *demo = head;
+	unsigned int i = 0, len = 0;
+
+	while (demo != NULL)
+	{
+		len++;
+		demo = demo->next;
+	}
+	if (index >= len)
+		return (NULL);
+	while (hd != NULL)
+	{
+		if (i == index)
+		{
+			return (hd);
+		}
+		i++;
+		hd = hd->next;
+	}
+	return (NULL);
+}
