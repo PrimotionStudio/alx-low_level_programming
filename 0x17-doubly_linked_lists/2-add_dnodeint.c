61e7c6bebb0f6d81cb8c9f5807a962a9343b8343
Martins <oyedelenewton@gmail.com>
2023-12-07 03:21:46 -0800

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
 	return (new);
 }
