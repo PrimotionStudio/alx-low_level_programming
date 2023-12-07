9a8d9fb453365c66098862f33e6b88059d799584
Martins <oyedelenewton@gmail.com>
2023-12-07 03:36:21 -0800

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+  * add_dnodeint - Used to add to the top of the list
+  * @head: the top
+  * @n: The value
+  * Return: The new list
+  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
