3cf34b8362dbf696a39d72f99c44580fb68ca25d
Martins <oyedelenewton@gmail.com>
2023-12-07 05:46:16 -0800

    6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,28 +7,12 @@
   */
 int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *p, *nx;
 	int sum = 0;
-
-	if ((head == NULL) && (head->prev == NULL))
-		return (sum);
-	if (head->prev != NULL)
+	
+	while (head != NULL)
 	{
-		p = head;
-		while (p->prev != NULL)
-		{
-			sum += p->n;
-			p = p->prev;
-		}
-	}
-	if (head->next != NULL)
-	{
-		nx = head;
-		while (nx != NULL)
-		{
-			sum += nx->n;
-			nx = nx->next;
-		}
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
