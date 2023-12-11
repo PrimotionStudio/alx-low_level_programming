864fec2fed56dbb6839958b04e22f3f568c71626
Martins <oyedelenewton@gmail.com>
2023-12-11 15:34:09 -0800

    0

---
+++ 0x18-dynamic_libraries/dynamics.c
@@ -0,0 +1,120 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+int _islower(int c)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+int _isalpha(int c)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+int _abs(int n)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+int _isupper(int c)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+int _isdigit(int c)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+int _strlen(char *s)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+void _puts(char *s)
+{
+    /* TODO: Implement the function */
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    /* TODO: Implement the function */
+    return (dest);
+}
+
+int _atoi(char *s)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+char *_strcat(char *dest, char *src)
+{
+    /* TODO: Implement the function */
+    return (dest);
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+    /* TODO: Implement the function */
+    return (dest);
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    /* TODO: Implement the function */
+    return (dest);
+}
+
+int _strcmp(char *s1, char *s2)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    /* TODO: Implement the function */
+    return (s);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    /* TODO: Implement the function */
+    return (dest);
+}
+
+char *_strchr(char *s, char c)
+{
+    /* TODO: Implement the function */
+    return (NULL);
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+    /* TODO: Implement the function */
+    return (0);
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+    /* TODO: Implement the function */
+    return (NULL);
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+    /* TODO: Implement the function */
+    return (NULL);
+}
