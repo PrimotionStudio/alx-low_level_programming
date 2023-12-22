1f7816ff46aa1a4554f4dc413ffbd53c71c6eacd
Martins <oyedelenewton@gmail.com>
2023-12-22 00:17:42 -0800

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 	new_ht->size = size;
-	new_ht->array = malloc(sizeof(hash_node_t) * size);
+	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_ht->array == NULL)
 		return (NULL);
 	while (i < size)
