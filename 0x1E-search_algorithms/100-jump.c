e024f90252b25d1ee2bc48a0232becb5f10a5b10
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-11 02:11:50 +0100

    linear search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,13 +11,33 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	while (base <= _size)
+	/*
+	Risk of infinite loop
+	*/
+	while (1)
 	{
 		/*
 		printf("base=>%d | top=>%d | size=>%d | jump_size=>%d | jumps=>%d\n", base, top, _size, jump_size, jumps);
 		*/
 		printf("Value checked array[%d] = [%d]\n", base, array[base]);
-		if (array[top] >= value)
+		if (top >= _size)
+		{
+
+			i = base;
+			
+			printf("base=>%d | top=>%d | size=>%d | jump_size=>%d | jumps=>%d\n", base, top, _size, jump_size, jumps);
+			
+			printf("Value found between indexes [%d] and [%d] ->4\n", base, top);
+			while (i <= top - _size)
+			{
+				printf("Value checked array[%d] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+					return (i);
+				i++;
+			}
+			break;
+		}
+		else if (array[top] >= value)
 		{
 			i = base;
 			/*
