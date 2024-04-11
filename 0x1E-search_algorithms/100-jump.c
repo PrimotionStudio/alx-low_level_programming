ebb666907bcf6f7077f084e0f0803c280e1a4ee5
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-11 02:59:03 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,34 +2,29 @@
 #include <math.h>
 #include <stddef.h>
 
+/**
+ * jump_search -  searches for a value in a sorted
+ * array of integers using the Jump search algorithm
+ *  @array: a pointer to the first element of the array to search in
+ *  @size: the number of elements in array
+ *  @value: the value to search for
+ *  Return: the index where value is located or if
+ *  value is not present in array or if array is NULL, -1
+ */
 int jump_search(int *array, size_t size, int value)
 {
 	int jump_size = (int)sqrt((double)size), i,
 	    jumps = 0, base = 0, top = base + jump_size,
 	    _size = (int)size;
 
-
 	if (array == NULL)
 		return (-1);
-	/*
-	Risk of infinite loop
-	*/
 	while (top <= _size)
 	{
-		/*
-		printf("base=>%d | top=>%d | size=>%d |
-		jump_size=>%d | jumps=>%d\n", base,
-		top, _size, jump_size, jumps);
-		*/
 		printf("Value checked array[%d] = [%d]\n", base, array[base]);
 		if (array[top] >= value)
 		{
 			i = base;
-			/*
-			printf("base=>%d | top=>%d | size=>%d |
-			jump_size=>%d | jumps=>%d\n", base,
-			top, _size, jump_size, jumps);
-			*/
 			printf("Value found between indexes [%d] and [%d]\n", base, top);
 			while (i <= top)
 			{
@@ -47,11 +42,6 @@ int jump_search(int *array, size_t size, int value)
 	if (top > _size)
 	{
 		i = base;
-		/*
-		printf("base=>%d | top=>%d | size=>%d |
-		jump_size=>%d | jumps=>%d\n", base,
-		top, _size, jump_size, jumps);
-		*/
 		printf("Value checked array[%d] = [%d]\n", base, array[base]);
 		printf("Value found between indexes [%d] and [%d]\n", base, top);
 		while (array[i] && i < _size)
@@ -61,7 +51,6 @@ int jump_search(int *array, size_t size, int value)
 				return (i);
 			i++;
 		}
-		return (-1);
 	}
 	return (-1);
 }
